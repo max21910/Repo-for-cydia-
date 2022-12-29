562d796aabf1018309faeedc909b15325b7a8d8f
Maxime <44231355+max21910@users.noreply.github.com>
2022-12-29 21:02:48 +0100

    add support for future possibilities

--- updaterepo.sh
+++ updaterepo.sh
@@ -1,2 +1,4 @@
+echo "update Started make shure Packages files  is in the folder"
 bzip2 -z /Users/maxime/Documents/GitHub/Repo-for-cydia-/Packages
 echo "Repository Updated, thanks for using Max-Studio!"
+
